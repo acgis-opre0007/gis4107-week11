e2148e344d0767d68e90434c93b078227b2206a8
Laura Oprescu <opre0007@algonquinlive.com>
2022-11-28 20:20:03 -0500

    tests passing up to question 12

--- LauraO_NathanM/shapes.py
+++ LauraO_NathanM/shapes.py
@@ -18,18 +18,44 @@ class Circle:
 
 
 class Square: 
-      def __init__(self):
+    def __init__(self):
         self.__side = ''
 
-        @property 
-        def side(self):
-            return self.__side 
+    @property 
+    def side(self):
+        return self.__side 
 
-      
+    @side.setter
+    def side(self, side):
+        self.__side = side
+        
+    @property
+    def area_sq(self):
+        return (self.__side * self.__side)
 
+class Rectangle: 
+    def __init__(self):
+        self.__length = ''
+        self.__width = ''
     
+    @property
+    def length(self):
+        return self.__length
+    
+    @length.setter
+    def length(self, length):
+        self.__length = length
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        self.__width = width
 
+    @property
+    def area_rec(self):
+        return (self.__length * self.__width)
 
-class Rectnage: 
-    pass
  
--- LauraO_NathanM/test_shapes.py
+++ LauraO_NathanM/test_shapes.py
@@ -1,9 +1,8 @@
-import shapes
 import pytest 
+import shapes
 
 
-
-def test_area_circle(circle):
+def test_area_circle():
     expected = 12.566
     circle = shapes.Circle() 
     circle.radius = 2 
@@ -12,10 +11,19 @@ def test_area_circle(circle):
     assert expected == pytest.approx(actual, 0.001)
 
 
-def test_area_square(square):
+def test_area_square():
     expected = 9
-    square = shapes.Square() 
-    square.side = 3 
-    actual = square.area_square 
+    square_1 = shapes.Square() 
+    square_1.side = 3 
+    actual = square_1.area_sq
 
-    assert expected == pytest.approx(actual, 0.001)
+    assert expected == actual
+
+def test_area_rectangle():
+    expected = 8
+    rectangle_1 = shapes.Rectangle()
+    rectangle_1.length = 4
+    rectangle_1.width = 2
+    actual = rectangle_1.area_rec
+
+    assert expected == actual
